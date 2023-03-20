489590330b1c87c6c633c7eed527e1068d91b2f2
denkom007 <denkom007@gmail.com>
2023-03-20 15:48:35 +0300

    updated configs

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'pgsql:host=localhost;dbname=orders_test',
+    'username' => 'dk007',
+    'password' => 'dk007',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
